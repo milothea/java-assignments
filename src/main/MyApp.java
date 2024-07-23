7a4b17ab416fa2318917fa54ef57b4ab58e803c2
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2024-07-24 00:27:47 +0200

    refactor: refactor main app method

--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -1,60 +1,48 @@
 package main;
 
-import main.ticket.Ticket;
+import main.constants.Sectors;
+import main.models.Price;
 import main.ticketService.TicketService;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 public class MyApp {
     public static void main(String[] args) {
+        String separator = "*******************";
+        Random random = new Random();
         TicketService ticketService = new TicketService();
-
-        String concertHall = "Cool place";
-        int eventCode = 123;
         LocalDateTime startTime = LocalDateTime.of(2024, 10, 1, 19, 30);
-        boolean isPromo = true;
-        char stadiumSector = 'B';
-        int maxWeight = 1500;
-        double price = 7.50;
-
-        Ticket emptyTicket = ticketService.buyTicket();
-        Ticket limitedTicket = ticketService.buyTicket(concertHall, eventCode, startTime);
-        Ticket fullTicket = ticketService.buyTicket(concertHall, eventCode, startTime, isPromo, stadiumSector, maxWeight, price);
-
-        System.out.println("Ticket created with no arguments and filled with default values:");
-        ticketService.printTicket(emptyTicket);
-        System.out.println("Ticket created with limited number of arguments and supplemented with default values:");
-        ticketService.printTicket(limitedTicket);
-        System.out.println("Ticket created with full list of arguments:");
-        ticketService.printTicket(fullTicket);
+        String[] concertHalls = {"Concert hall", "Nightclub", "Stadium"};
+        int[] maxWeights = {1000, 2100, 3500};
+        int[] eventCodes = {123, 456, 789};
+        char[] sectors = Sectors.values();
 
         try {
-            int wrongEventCode = 4567;
+            for (int i = 0; i < 10; i++) {
+                Price price = new Price(7.50 + random.nextInt(10));
+                int randomIdx = random.nextInt(3);
+                boolean isPromo = randomIdx % 2 == 0;
 
-            System.out.println("Ticket created with wrong number of digits for event code:");
-            ticketService.buyTicket(concertHall, wrongEventCode, startTime, isPromo, stadiumSector, maxWeight, price);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+                ticketService.buyTicket(
+                        concertHalls[randomIdx],
+                        eventCodes[randomIdx],
+                        startTime, isPromo,
+                        sectors[randomIdx],
+                        maxWeights[randomIdx],
+                        price
+                );
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
-        try {
-            String wrongConcertHall = "Some super cool place with a very long name";
-
-            System.out.println("Ticket created with too long concert hall name:");
-            ticketService.buyTicket(wrongConcertHall, eventCode, startTime, isPromo, stadiumSector, maxWeight, price);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            char wrongStadiumSector = 'D';
-
-            System.out.println("Ticket created with a wrong stadium sector:");
-            ticketService.buyTicket(concertHall, eventCode, startTime, isPromo, wrongStadiumSector, maxWeight, price);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-
+        ticketService.printTickets();
+        System.out.println(separator);
+        System.out.println("Print tickets by ID - 1, 3, 5");
+        System.out.println(separator);
+        ticketService.printTicketById(1);
+        ticketService.printTicketById(3);
+        ticketService.printTicketById(5);
     }
 }
