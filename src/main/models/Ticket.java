563befd73969186b6cc1861861552b0f0e3e427d
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2024-07-29 23:37:03 +0200

    refactor: refactor Ticket class

--- src/main/models/Ticket.java
+++ src/main/models/Ticket.java
@@ -3,16 +3,15 @@ package main.models;
 import main.constants.DefaultValues;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class Ticket {
-    private static int ticketsCounter = 0;
+public abstract class Ticket {
     private final int id;
-    private String concertHall = DefaultValues.EVENT_VENUE;
+    private static int ticketsCounter = 0;
+    private String venuePlace = DefaultValues.EVENT_VENUE;
     private int eventCode = DefaultValues.EVENT_CODE;
-    private LocalDateTime startTime;
+    protected LocalDateTime startTime;
     private boolean isPromo = DefaultValues.IS_PROMO;
-    private char stadiumSector = DefaultValues.STADIUM_SECTOR;
-    private int maxBackpackWeight = DefaultValues.MAX_BACKPACK_WEIGHT;
     private Price ticketPrice = new Price(DefaultValues.TICKET_PRICE);
 
     private final LocalDateTime orderDate;
@@ -23,18 +22,16 @@ public class Ticket {
         this.orderDate = LocalDateTime.now();
     }
 
-    public Ticket(String concertHall, int eventCode, LocalDateTime startTime) {
+    public Ticket(String venuePlace, int eventCode, LocalDateTime startTime) {
         this();
-        this.concertHall = concertHall;
+        this.venuePlace = venuePlace;
         this.eventCode = eventCode;
         this.startTime = startTime;
     }
 
-    public Ticket(String concertHall, int eventCode, LocalDateTime startTime, boolean isPromo, char stadiumSector, int maxBackpackWeight, Price ticketPrice) {
-        this(concertHall, eventCode, startTime);
+    public Ticket(String venuePlace, int eventCode, LocalDateTime startTime, boolean isPromo, Price ticketPrice) {
+        this(venuePlace, eventCode, startTime);
         this.isPromo = isPromo;
-        this.stadiumSector = stadiumSector;
-        this.maxBackpackWeight = maxBackpackWeight;
         this.ticketPrice = ticketPrice;
     }
 
@@ -46,12 +43,8 @@ public class Ticket {
         return this.eventCode;
     }
 
-    public String getConcertHall() {
-        return this.concertHall;
-    }
-
-    public char getStadiumSector() {
-        return this.stadiumSector;
+    public String getVenuePlace() {
+        return this.venuePlace;
     }
 
     public LocalDateTime getStartTime() {
@@ -60,10 +53,6 @@ public class Ticket {
 
     public boolean getIsPromo() {
         return this.isPromo;
-    }
-
-    public int getMaxBackpackWeight() {
-        return this.maxBackpackWeight;
     }
 
     public LocalDateTime getOrderDate() {
@@ -78,7 +67,59 @@ public class Ticket {
         return ticketsCounter;
     }
 
-    public void setTicketsCounter(int value) {
+    private void setTicketsCounter(int value) {
         this.ticketsCounter = value;
+    }
+
+    protected String parseDateToString(LocalDateTime date) {
+        if (date == null) {
+            return "TBC";
+        }
+
+        int minutes = date.getMinute();
+        String parsedMinutes = minutes >= 10 ? Integer.toString(minutes) : '0' + Integer.toString(minutes);
+
+        return date.getDayOfMonth() + "-" +
+                date.getMonthValue() + "-" +
+                date.getYear() + " " +
+                date.getHour() + ":" +
+                parsedMinutes;
+    }
+
+    public void shared(String mail) {
+        System.out.println("Ticket with ID " + this.getId() + " is shared by mail: " + mail);
+    }
+
+    public void shared(int phone) {
+        System.out.println("Ticket with ID " + this.getId() + " is shared by phone: " + phone);
+    }
+
+    public abstract Ticket getTicketData();
+
+    public abstract void print();
+
+    @Override
+    public String toString() {
+        return "Ticket with ID " + this.getId() + " for event No. " + this.getEventCode();
+    }
+
+    public int hashCode() {
+        return Objects.hash(id, venuePlace, eventCode, startTime, isPromo, orderDate, ticketPrice);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        Ticket ticket = (Ticket) obj;
+
+        return this.id == ticket.id &&
+                this.venuePlace.equals(ticket.venuePlace) &&
+                this.eventCode == ticket.eventCode &&
+                this.startTime.equals(ticket.startTime) &&
+                this.isPromo == ticket.isPromo &&
+                this.orderDate.equals(ticket.orderDate) &&
+                this.ticketPrice.equals(ticket.ticketPrice);
     }
 }
